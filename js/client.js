807f29a657f611659284875e43f8908b2ccfcf85
edipascale <dipascae@tcd.ie>
2017-04-25 17:37:36 +0100

    managed to get the deployed helper to work, but event listening seems to be running synchronously and thus hanging the client.

--- js/client.js
+++ js/client.js
@@ -1,4 +1,42 @@
-
+// console.log(sla_artifacts);
+// load web3 to interact with the blockchain
+var Web3 = require('web3')
+if (typeof web3 != 'undefined') {
+  web3 = new Web3(web3.currentProvider);
+} else {
+  web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+}
+var accounts = web3.eth.accounts;
+var owner = accounts[0];
+var provider = accounts[1];
+// deploy contract and register provider
+const sla_artifacts = require('../build/contracts/SLA.json')
+const contract = require('truffle-contract')
+let SLA = contract(sla_artifacts);
+SLA.setProvider(web3.currentProvider);
+var sla;
+return SLA.deployed().then(function(instance){
+  sla = instance;
+  return sla.registerProvider(provider, 0, 0, 1, {from: owner});
+}).then(function(registeredTx){
+  console.log("Balance of account 0: %s", web3.eth.getBalance(accounts[0]).toString(10));
+  console.log("Balance of account 1: %s", web3.eth.getBalance(accounts[1]).toString(10));
+  // watch for payout notifications
+  return payoutEvent = sla.PeriodicPayout([{_provider: provider}], function(error, result){
+    if (!error) {
+      console.log('New PeriodicPayout event caught for an average throughput of %d Kbps, withdrawing', result.args._trafficInKb);
+      sla.withdraw({from: provider}).then(function(withdrawTx){
+        console.log("New balance of account 1: %s", web3.eth.getBalance(accounts[1]).toString(10));
+      }).catch(function(error) {
+        console.log(error);
+      });
+    } else {
+      console.log(error);
+    }
+  });
+}).catch(function(error){
+  console.log(error);
+});
 // Enter here the name of the node you want to listen to (eg dub, bcn0, bcn1, etc.)
 node_name = "default"
 time_interval = 1 * 5 * 1000; // interval in ms over which compute the throughput
@@ -29,4 +67,15 @@ function avgThroughput() {
   var avg_t = tput_values.reduce((x, y) => x + y) / tput_values.length;
   console.log('Avg throughput over %d seconds: %d', time_interval / 1000, avg_t)
   tput_values = [];
+  if (avg_t > 0)  {
+    payout(avg_t);
+  }
+}
+
+function payout(throughput) {
+  return SLA.deployed().then(function(instance) {
+    instance.notifyPayment(provider, throughput * 1024, {from: owner});
+  }).catch(function(error){
+    console.log(error);
+  });
 }
