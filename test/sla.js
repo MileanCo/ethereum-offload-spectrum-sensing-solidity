fc2c528c5d16580f9d5cf2e5ceeb9d0122886198
edipascale <dipascae@tcd.ie>
2017-04-26 17:25:12 +0100

    attempting to fix the withdraw unit test. Seems like the cost of the transaction is way higher than the reward we are getting, but even when attempting to take that into account the maths are off. Gotta check this better

--- test/sla.js
+++ test/sla.js
@@ -1,6 +1,9 @@
 var SLA = artifacts.require("./SLA.sol");
 
 contract('SLA', function(accounts) {
+
+  const _costPerKbps = 100;
+
   it("should save the address of the creator as the _owner", function() {
     return SLA.deployed().then(function(instance) {
       return instance._owner.call({from: accounts[0]});
@@ -14,7 +17,7 @@ contract('SLA', function(accounts) {
 
   it("should reject a provider registration with non-positive _costPerKbps", function() {
     return SLA.deployed().then(function(instance) {
-      return instance.registerProvider(accounts[0], 0, 0, 1,
+      return instance.registerProvider(accounts[1], 0, 0, 1,
                                        {from: accounts[0]});
     }).then(function(tx_id) {
       for (var i = 0; i < tx_id.logs.length; i++) {
@@ -31,7 +34,7 @@ contract('SLA', function(accounts) {
 
   it("should reject a provider registration with non-positive _penaltyPerKbps", function() {
     return SLA.deployed().then(function(instance) {
-      return instance.registerProvider(accounts[0], 0, 1, 0,
+      return instance.registerProvider(accounts[1], 0, 1, 0,
                                        {from: accounts[0]});
     }).then(function(tx_id) {
       for (var i = 0; i < tx_id.logs.length; i++) {
@@ -48,14 +51,14 @@ contract('SLA', function(accounts) {
 
   it("should register a new provider if invoked by the owner", function() {
     return SLA.deployed().then(function(instance) {
-      return instance.registerProvider(accounts[0], 0, 1, 1,
+      return instance.registerProvider(accounts[1], 0, _costPerKbps, 1,
                                        {from: accounts[0]});
     }).then(function(tx_id){
       var found = false;
       for (var i = 0; i < tx_id.logs.length; i++) {
         var log = tx_id.logs[i];
         if (log.event == "RegisteredProvider") {
-          assert.equal(log.args._provider, accounts[0]);
+          assert.equal(log.args._provider, accounts[1]);
           found = true;
           break;
         }
@@ -70,14 +73,14 @@ contract('SLA', function(accounts) {
   it("should fire event and update pendingWithdrawals when notifyPayment is called", function() {
     var amount = 10;
     var instance;
-    var account_zero_starting_pending;
-    var account_zero_ending_pending;
+    var account_one_starting_pending;
+    var account_one_ending_pending;
     return SLA.deployed().then(function(inst) {
       instance = inst;
-      return instance.pendingWithdrawals.call(accounts[0]);
+      return instance.pendingWithdrawals.call(accounts[1]);
     }).then(function(pending){
-      account_zero_starting_pending = pending.toNumber();
-      return instance.notifyPayment(accounts[0], amount, {from: accounts[0]});
+      account_one_starting_pending = pending.toNumber();
+      return instance.notifyPayment(accounts[1], amount, {from: accounts[0]});
     }).then(function(notify_txid){
       var found = false;
       for (var i = 0; i < notify_txid.logs.length; i++) {
@@ -89,10 +92,49 @@ contract('SLA', function(accounts) {
         }
       }
       assert.isTrue(found);
-      return instance.pendingWithdrawals.call(accounts[0]);
+      return instance.pendingWithdrawals.call(accounts[1]);
     }).then(function(newPending){
-      account_zero_ending_pending = newPending.toNumber();
-      assert.equal(account_zero_ending_pending, account_zero_starting_pending + amount);
+      account_one_ending_pending = newPending.toNumber();
+      assert.equal(account_one_ending_pending, account_one_starting_pending + amount * _costPerKbps);
+    }).catch(function(error){
+      console.log(error);
+      assert.fail();
+    });
+  });
+
+
+  it("should allow withdrawal and remove pending funds when withdraw", function() {
+    //TODO: check that the contract was charged, and that no InsufficientFunds was thrown
+    // var account_zero_starting_balance;
+    // var account_zero_ending_balance;
+    var account_one_starting_balance;
+    var account_one_ending_balance;
+    var account_one_starting_pending;
+    return SLA.deployed().then(function(inst) {
+      instance = inst;
+      // account_zero_starting_balance = web3.eth.getBalance(accounts[0]);
+      account_one_starting_balance = web3.eth.getBalance(accounts[1]);
+      console.log("starting balance", account_one_starting_balance.toString(10)); // web3.fromWei(account_one_starting_balance, "ether"));
+      return instance.pendingWithdrawals.call(accounts[1]);
+    }).then(function(pending){
+      account_one_starting_pending = pending;
+      console.log("pending withdrawal", account_one_starting_pending.toString(10));
+      return instance.increaseFunds({from: accounts[0], value: 2*pending});
+    }).then(function(funds_txid){
+      return instance.withdraw({from: accounts[1]});
+    }).then(function(withdraw_txid){
+      // console.log("transaction receipt: ", withdraw_txid);
+      // account_zero_ending_balance = web3.eth.getBalance(accounts[0]);
+      var tx_cost = withdraw_txid.receipt.gasUsed * web3.eth.gasPrice;
+      console.log("transaction cost", tx_cost.toString(10));
+      account_one_ending_balance = web3.eth.getBalance(accounts[1]);
+      console.log("ending balance", account_one_ending_balance.toString(10)); // web3.fromWei(account_one_ending_balance, "ether"));
+      // assert.equal(account_zero_starting_balance - account_zero_ending_balance, new BigNumber(10), "account zero was not charged")
+      var account_one_credit = account_one_ending_balance.minus(account_one_starting_balance);
+      assert.equal(account_one_credit, account_one_starting_pending.minus(tx_cost), "account one was not credited");
+      return instance.pendingWithdrawals.call(accounts[1]);
+    }).then(function(newPending){
+      assert.equal(newPending.toNumber(), 0);
     }).catch(function(error){
       console.log(error);
       assert.fail();
@@ -101,7 +143,7 @@ contract('SLA', function(accounts) {
 
   it("should reject a second registration for the same provider", function() {
     return SLA.deployed().then(function(instance) {
-      return instance.registerProvider(accounts[0], 0, 1, 1,
+      return instance.registerProvider(accounts[1], 0, 1, 1,
                                        {from: accounts[0]});
     }).then(function(tx_id) {
       for (var i = 0; i < tx_id.logs.length; i++) {
@@ -118,7 +160,7 @@ contract('SLA', function(accounts) {
 
   it("should remove a reigstered provider after it has been blocked", function() {
     return SLA.deployed().then(function(instance) {
-      return instance.blockProvider(accounts[0], {from: accounts[0]});
+      return instance.blockProvider(accounts[1], {from: accounts[0]});
     }).then(function(tx_id) {
       var found = false;
       for (var i = 0; i < tx_id.logs.length; i++) {
@@ -137,12 +179,5 @@ contract('SLA', function(accounts) {
   });
 
   it("should throw when we try to block an unregistered provider");
-
-  var account_zero_starting_balance;
-  var account_zero_ending_balance;
-  var account_one_starting_balance;
-  var account_one_ending_balance;
-
-  it("should allow withdrawal and remove pending funds when withdraw");
 
 });
