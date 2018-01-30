c7695cd94c8a5ea395328db46ab697f84f919397
jakobwiren <34477079+jakobwiren@users.noreply.github.com>
2018-01-30 13:24:31 +0100

    Method files

---
+++ evaluate.m
@@ -0,0 +1,9 @@
+function [ cheating_record ] = evaluate( helper_data, cheating_record, index, result )
+%EVALUATE Summary of this function goes here
+%   Detailed explanation goes here
+    
+
+
+
+end
+
